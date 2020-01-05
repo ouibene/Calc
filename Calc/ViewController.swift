d41ff04707f15b4ff2e5e76b6916c984ef70c7f0
oui <ouibene@gmail.com>
2020-01-05 20:13:54 +0900

    initial commit

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftField: UITextField!
+    @IBOutlet weak var rightField: UITextField!
+    
+    @IBAction func process(_ sender: Any) {
+ 
+        let leftStr = leftField.text!
+        let rightStr = rightField.text!
+        
+        //Type conversion
+        let leftNum = Int(leftStr)!
+        let rightNum = Int(rightStr)!
+        
+        //Alert
+        let msg = "\(leftNum) + \(rightNum) = \(leftNum + rightNum)"
+        let alert = UIAlertController(title: "결과", message: msg, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        leftField.becomeFirstResponder() // <-> resignFirstResponder()
     }
 
-
 }
-
